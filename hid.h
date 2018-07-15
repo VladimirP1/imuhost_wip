1ab551e2ecfdd972b5a9a3f687b78c73c25aea26
vladimir <vladimir.pinchuk01@gmail.com>
2018-07-15 12:57:42 +0300

    Initial

---
+++ hid.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <hidapi.h>
+#include <string>
+#include <gsl-lite.hpp>
+
+using namespace std;
+
+class HID_exception
+{
+    string desc;
+public:
+    HID_exception(string desc) : desc(desc) {}
+    string get_desc(){ return desc; }
+};
+
+class HID
+{
+    hid_device *dev_handle = nullptr;
+public:
+    HID();
+    ~HID();
+
+    void open(uint16_t pid, uint16_t vid);
+    int read(gsl::span<uint8_t> data);
+    int write(gsl::span<uint8_t> data);
+    int set_feature(gsl::span<uint8_t> data);
+    int get_feature(gsl::span<uint8_t> data);
+    void set_nonblocking(bool is_nonblock);
+    void close();
+};
